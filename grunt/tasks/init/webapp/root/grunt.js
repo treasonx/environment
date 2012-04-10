a1b44fd9b16dbeec6bce78e453e912c8be272025
James Morrin <treasonx@gmail.com>
2012-04-10 12:02:05 -0400

    added dev static server

--- grunt/tasks/init/webapp/root/grunt.js
+++ grunt/tasks/init/webapp/root/grunt.js
@@ -1,6 +1,25 @@
-
+var connect = require('connect');
 
 module.exports = function(grunt) {
+
+  //serve webpage for development
+  grunt.registerTask('deverver', 'start a server to serve static files for development', function() {
+    var app = connect(),
+        done = this.async();
+    grunt.log.writeln('Serving Static Files on port 3000 ctrl-c to exit...');
+    process.on('SIGINT', function() {
+      grunt.log.writeln('Stopping Server...');
+      done();
+    });
+    app.use(connect.favicon())
+            .use(connect.logger())
+            .use(connect.static(__dirname + '/'))
+            .listen(3000);
+  });
+
+
+
+
   grunt.initConfig({
     pkg: '<json:package.json>',
     server: {
@@ -8,4 +27,7 @@ module.exports = function(grunt) {
       base: '.'
     }
   });   
+
+
+
 };
